03f892885a99da8ad216382461c97393fba112a9
João Vitor Silva Machado <86895133020@senacrs.edu.br>
2025-04-03 10:26:03 -0300

    fiinal

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-const menos1 = document.getElementById('menos1')
-const mais1 = document.getElementById('mais1')
-const contador = document.getElementById('contador')
-const zerar = document.getElementById('zerar')
- 
-const updatecontador = () => {
+const menos1 = document.getElementById('menos1');
+const mais1 = document.getElementById('mais1');
+const contador = document.getElementById('contador');
+const zerar = document.getElementById('zerar');
+
+let count = 0;
+let intervalId = null;
+
+const updateContador = () => {
     contador.innerHTML = count;
 };
 
-let count = 0;
-let intervalId = 0;
-
-mais1.addEventListener('mousedown', () => {
-    intervalId = setInterval (() =>{
-        count += 1;
-        updatecontador();
-    },200)
-})
-
-menos1.addEventListener('mousedown', () => {
-    intervalId = setInterval (() =>{
-        count  -= 1;
-        if(count < 0){
-            count = 0;
-        }
-        updatecontador();
-    }, 200)
+mais1.addEventListener('click', () => {
+    count += 1;
+    updateContador();
 });
 
-zerar.addEventListener('click', () => {  
+
+menos1.addEventListener('click', () => {
+    count -= 1;
+    if (count < 0) count = 0; 
+    updateContador();
+});
+
+
+mais1.addEventListener('mousedown', () => startCounting('increment'));
+
+
+menos1.addEventListener('mousedown', () => startCounting('decrement'));
+
+
+document.addEventListener('mouseup', () => {
+    clearInterval(intervalId);
+});
+
+
+zerar.addEventListener('click', () => {
     count = 0;
-    updatecontador();
- 
+    updateContador();
 });
-
-document.addEventListener('mouseup', () => clearInterval(intervalId));
-
-
-
